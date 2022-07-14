590b9ab7d93af44ddda4d134e24d99d3900f9888
Darnell Champen <zerosalvation@gmail.com>
2022-07-14 11:55:51 -0400

    - removed some imports.

--- client/src/components/pages/introScreen.jsx
+++ client/src/components/pages/introScreen.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 import MainScreen from './mainScreen';
 import AuthenticationScreen from './authenticationScreen';
