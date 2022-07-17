6c71de63b7ce2a6e57ebdbf930ede8d8d9ae1822
Darnell Champen <zerosalvation@gmail.com>
2022-07-17 10:52:49 -0400

    - added blur to main screen when search completed.

--- client/src/components/pages/accountDetailsScreen.tsx
+++ client/src/components/pages/accountDetailsScreen.tsx
@@ -3,57 +3,40 @@ import { useLocation } from 'react-router-dom';
 
 import NavBar from '../navBar'
 import Avatar from '../ui/avatar'
+import Card from '../ui/card';
+import Button from '../ui/button';
 
 import { useLoggedInUser } from '../../hooks/useLoggedInUser';
 import { tryAddToStorage } from '../../helper/storageHelper';
+import { INavLinks } from '../../interfaces/INavLinks';
+import { IUser } from '../../interfaces/IUser';
 
 import '../../styles/inputs.css';
 import '../../styles/accountDetailsScreen.css';
-import { INavLinks } from '../../interfaces/INavLinks';
-import { IUser } from '../../interfaces/IUser';
-import { IUserState } from '../../interfaces/IUserState';
-
-const AccountDetailsUpdateForm: React.FC<IUserState> = ({ user, setUser }) => {
-    const [updatedUser, setUpdatedUser] = useState(user);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setUpdatedUser({ ...updatedUser, [name]: value });
-    }
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        setUser(updatedUser);
-    }
-
-    return (
-        <>
-            <form className="account-details-update-form" onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="firstName">User Name</label>
-                    <input className='text-input' type="text" id="userName" name="userName" value={updatedUser.userName} onChange={handleChange} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="firstName">First Name</label>
-                    <input className='text-input' type="text" id="firstName" name="firstName" value={updatedUser.firstName} onChange={handleChange} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="lastName">Last Name</label>
-                    <input className='text-input' type="text" id="lastName" name="lastName" value={updatedUser.lastName} onChange={handleChange} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input className='text-input' type="email" id="email" name="email" value={updatedUser.email} onChange={handleChange} />
-                </div>
-                <input type="submit" value="Update" />
-            </form>
-        </>
-    )
-}
 
 const AccountDetailsScreen: React.FC<INavLinks> = ({ navLinks }) => {
     const [user, setUser] = useState({} as IUser);
     const loggedInUser = useLoggedInUser(useLocation());
+
+    const updateableAccountOptions = [
+        {
+            key: 'Username:',
+            value: `${user.userName}`,
+        },
+        {
+            key: 'Email:',
+            value: `${user.email}`,
+        },
+        {
+            key: 'Mobile Phone Number:',
+            value: '+1 (202) 555-1212',
+        },
+        {
+            key: 'Profile Picture:',
+            value: '',
+        }
+    ]
+
     useEffect(() => {
         setUser(loggedInUser);
     }, [loggedInUser]);
@@ -67,7 +50,17 @@ const AccountDetailsScreen: React.FC<INavLinks> = ({ navLinks }) => {
             <div className="main-screen-body">
                 <NavBar user={user} setUser={setUser} />
                 <Avatar navLinks={navLinks} />
-                <AccountDetailsUpdateForm user={user} setUser={setUser} />
+                <div className='updatable-account-options'>
+                    {updateableAccountOptions.map((option, index) => (
+                        <Card className='updatable-account-option' key={index}>
+                            <div className='updatable-account-option-data'>
+                                <h3>{option.key}</h3>
+                                <p>{option.value}</p>
+                            </div>
+                            <Button label='Edit' />
+                        </Card>
+                    ))}
+                </div>
             </div>
         </div>
     )
